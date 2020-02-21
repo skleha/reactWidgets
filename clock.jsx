994aa9104df0d2fb477682169b5d5866398027ca
thejenkdog <thejenkdog@gmail.com>
2020-02-21 11:17:04 -0800

    Complete clock

--- clock.jsx
+++ clock.jsx
@@ -4,20 +4,31 @@ class Clock extends React.Component {
 
   constructor(props) {
     super(props) 
-    this.state = new Date();
+    this.state = { time: new Date()};
+    this.tick = this.tick.bind(this);
+  }
 
+  tick() {
+    this.setState({time: new Date()})
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(this.tick, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intevalId);
   }
 
   render() {
     
-    const time = this.state.toString();
-
+    const hours = this.state["time"].getHours();
+    const minutes = this.state["time"].getMinutes();
+    const seconds = this.state["time"].getSeconds();
 
     return (
       <div>
-        <h3>Current time:</h3>
-        <h4>{time}</h4>
-
+        <h4>Current time: {hours}:{minutes}:{seconds}</h4>
       </div>
     )
   
